3192b519b1ec9317a139cc62e57a0cd16b2cb3f6
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-07-26 13:49:05 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,55 @@
 #include <iostream>
 #include <vector>
+// #include <cstdlib>
 
 using namespace std;
 
-vector<int> jing;
+int jing[9] = {0};
+
+char pring_jing_words(int number)
+{
+    if (number == 0)
+    {
+        return ' ';
+    }
+    else if (number == 1)
+    {
+        return 'X';
+    }
+    else if (number == 2)
+    {
+        return 'O';
+    }
+    else
+    {
+        cout << endl
+             << "ERROR:棋盘内容出现错误" << endl;
+        system("pause");
+        exit(-1);
+    }
+}
+
+void print_jing()
+{
+    system("cls");
+    cout << " 1 " << pring_jing_words(jing[0]) << " |";
+    cout << " 2 " << pring_jing_words(jing[1]) << " |";
+    cout << " 3 " << pring_jing_words(jing[2]) << " |";
+    cout << endl;
+    cout << " 4 " << pring_jing_words(jing[3]) << " |";
+    cout << " 5 " << pring_jing_words(jing[4]) << " |";
+    cout << " 6 " << pring_jing_words(jing[5]) << " |";
+    cout << endl;
+    cout << " 7 " << pring_jing_words(jing[6]) << " |";
+    cout << " 8 " << pring_jing_words(jing[7]) << " |";
+    cout << " 9 " << pring_jing_words(jing[8]) << " |";
+    cout << endl;
+}
 
 int main()
 {
     system("chcp 65001");
+    print_jing();
     system("pause");
     return 0;
 }
